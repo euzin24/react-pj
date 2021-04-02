95ad1ca03664e346e7be482e0f9bceda7ebedd63
euzin24 <euzin24@gmail.com>
2021-04-02 23:38:49 +0900

    App1 App2 분리

---
+++ src/App2.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Component } from 'react';
+
+class App2 extends Component{
+  render(){
+    console.log("App2 is rendered");
+    return(
+      <div>
+          <h1>Hello React</h1>
+      </div>
+    );
+  }
+}
+
+export default App2;
